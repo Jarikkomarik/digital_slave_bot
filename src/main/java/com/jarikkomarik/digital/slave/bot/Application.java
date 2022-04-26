78f98174f7b66f8d8a6275bd3f5d101c45d5adab
Jarikkomarik <ufland.ufland@gmail.com>
2022-04-26 21:34:46 +0200

    first commit

---
+++ src/main/java/com/jarikkomarik/digital/slave/bot/Application.java
@@ -0,0 +1,13 @@
+package com.jarikkomarik.digital.slave.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
