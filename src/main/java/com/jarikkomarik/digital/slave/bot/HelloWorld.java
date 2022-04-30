be973b3548dbfa2f0fb279f07e0af1e06306fbe7
Jarikkomarik <ufland.ufland@gmail.com>
2022-04-30 12:47:51 +0200

    get
    get

---
+++ src/main/java/com/jarikkomarik/digital/slave/bot/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.jarikkomarik.digital.slave.bot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return  "Hello world!";
+    }
+}
